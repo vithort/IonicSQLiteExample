a1b2f8357f813a7a6a29f90ea1734b364fbecd71
vithort <vithort@gmail.com>
2018-09-13 11:39:50 -0300

    Initial Commit
    Start:
    > ionic start IonicSQLiteExample blank
    
    Install SQLite:
    > ionic cordova plugin add cordova-sqlite-storage
    > npm install --save @ionic-native/sqlite
    
    Adicionar Plataforma:
    > ionic cordova platform add android
    
    Registrar o Provider SQLite;
    Registrar LOCALE_ID para customização de valores (R$);
    
    Criar Providers e Page:
    > ionic g provider database
    > ionic g provider product
    > ionic g provider category
    > ionic g page edit-product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import { SQLite } from '@ionic-native/sqlite';
+import { DatabaseProvider } from '../providers/database/database';
+import { CategoryProvider } from '../providers/category/category';
+import { ProductProvider } from '../providers/product/product';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: LOCALE_ID, useValue: 'pt-BR'},
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SQLite,
+    DatabaseProvider,
+    CategoryProvider,
+    ProductProvider
   ]
 })
 export class AppModule {}
