e9635f588141118e99d3f10c807642433724205e
vithort <vithort@gmail.com>
2018-09-13 15:15:04 -0300

    Commit Final
    :)

--- src/providers/category/category.ts
+++ src/providers/category/category.ts
@@ -1,17 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SQLiteObject } from '@ionic-native/sqlite';
+import { DatabaseProvider } from '../database/database';
 
-/*
-  Generated class for the CategoryProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class CategoryProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello CategoryProvider Provider');
-  }
+  constructor(private dbProvider: DatabaseProvider) { }
 
+  public getAll() {
+    return this.dbProvider.getDB()
+      .then((db: SQLiteObject) => {
+
+        return db.executeSql('select * from categories', [])
+          .then((data: any) => {
+            if (data.rows.length > 0) {
+              let categories: any[] = [];
+              for (var i = 0; i < data.rows.length; i++) {
+                var category = data.rows.item(i);
+                categories.push(category);
+              }
+              return categories;
+            } else {
+              return [];
+            }
+          })
+          .catch((e) => console.error(e));
+      })
+      .catch((e) => console.error(e));
+  }
 }
--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -1,17 +1,61 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
-/*
-  Generated class for the DatabaseProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DatabaseProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(
+    private sqlite: SQLite
+  ) {
     console.log('Hello DatabaseProvider Provider');
+  }
+
+  public getDB() {
+    return this.sqlite.create({
+      name: 'products.db',
+      location: 'default'
+    });
+  }
+
+  public createDatabase() {
+    return this.getDB()
+      .then((db: SQLiteObject) => {
+        this.createTables(db);
+
+        this.insertDefaultItems(db);
+      })
+      .catch(e => console.error(e));
+  }
+
+  private createTables(db: SQLiteObject) {
+    // Criando as tabelas
+    db.sqlBatch([
+      ['CREATE TABLE IF NOT EXISTS categories (id integer primary key AUTOINCREMENT NOT NULL, name TEXT)'],
+      ['CREATE TABLE IF NOT EXISTS products (id integer primary key AUTOINCREMENT NOT NULL, name TEXT, price REAL, duedate DATE, active integer, category_id integer, FOREIGN KEY(category_id) REFERENCES categories(id))']
+    ])
+      .then(() => console.log('Tabelas criadas'))
+      .catch(e => console.error('Erro ao criar as tabelas', e));
+  }
+
+  private insertDefaultItems(db: SQLiteObject) {
+    db.executeSql('select COUNT(id) as qtd from categories', [])
+      .then((data: any) => {
+        //Se não existe nenhum registro
+        if (data.rows.item(0).qtd == 0) {
+
+          // Criando as tabelas
+          db.sqlBatch([
+            ['insert into categories (name) values (?)', ['Hambúrgueres']],
+            ['insert into categories (name) values (?)', ['Bebidas']],
+            ['insert into categories (name) values (?)', ['Sobremesas']]
+          ])
+            .then(() => console.log('Dados padrões incluídos'))
+            .catch(e => console.error('Erro ao incluir dados padrões', e));
+
+        }
+      })
+      .catch(e => console.error('Erro ao consultar a qtd de categorias', e));
   }
 
 }
